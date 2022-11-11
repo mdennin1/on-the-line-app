868dc7f9d94c1f138369257994678d3684a2fb92
Danny Dennin <mdennin1@gmail.com>
2022-11-11 16:58:53 -0500

    added ts and fixed? schema import???

--- index.js
+++ index.js
@@ -1,53 +1,8 @@
-/*
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { resolvers, typeDefs } from "./graphql/schema";
-*/
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { PrismaClient } from "@prisma/client";
-import { gql } from 'apollo-server';
+import schema from "./graphql/schema.js"; //import { resolvers, typeDefs } from "./graphql/schema";
 
-const prisma = new PrismaClient();
-
-const typeDefs = gql`
-type User{
-    id: ID!
-    username: String!
-    email: String!
-    firstName: String
-    lastName: String
-    city: String
-    street: String
-    zip: String
-}
-
-type Query {
-    getAllUsers: {
-        User{
-            id
-            __typeName
-            username
-            email
-            firstName
-            lastName
-            city
-            street
-            zip
-        }
-    }
-}
-`;
-
-const resolvers = {
-    Query: {
-      getAllUsers: async (parent, args, context, info) => { 
-        return await prisma.user.findMany();
-      },
-    }
-};
-
-const server = new ApolloServer({ resolvers, typeDefs });
+const server = new ApolloServer(schema);
 const port = process?.env?.PORT ?? 4000;
 const {url} = async () => await startStandaloneServer(server, { context: () => console.log(`server listening on port ${port}`), listen: { port } });
 console.log(`**server url => ${url}`);

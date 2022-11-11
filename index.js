11d9102296e5b4fd3f8a43d72c94c405b81089ae
Danny Dennin <mdennin1@gmail.com>
2022-11-11 14:48:10 -0500

    trying stuff

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-import { ApolloServer } from "@apollo/server";
+import { ApolloServer, gql } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from "./graphql/typeDefs";
-import { resolvers } from "./graphql/resolvers";
+// import { typeDefs } from "./graphql/typeDefs";
+// import { resolvers } from "./graphql/resolvers";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+const typeDefs = gql`
+type User{
+    id: ID!
+    username: String!
+    email: String!
+    firstName: String
+}
+
+type Query {
+    getAllUsers: {
+        User{
+            id
+            __typeName
+            username
+            email
+            firstName
+        }
+    }
+}
+`;
+
+const resolvers = {
+  Query: {
+    getAllUsers: async () => await prisma.user.findMany()
+  }
+};
 
 const server = new ApolloServer({ resolvers, typeDefs });
 const {url} = await startStandaloneServer(server, {context: ()=>{}, listen: { port: 4000 }});
